f7d656e1fd28893232554c35fe1725726772ef56
Amritanshi <145573325+Amritanshi@users.noreply.github.com>
2026-05-22 12:42:53 +0530

    Add files via upload

---
+++ q25.py
@@ -0,0 +1,9 @@
+# Program to find common elements between two sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+common = set1.intersection(set2)
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+print("Common elements:", common)
