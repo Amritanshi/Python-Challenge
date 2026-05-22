49e1ec1afedf5ccb6f68c70027c5cf439245931e
Amritanshi <145573325+Amritanshi@users.noreply.github.com>
2026-05-22 12:41:08 +0530

    Add files via upload

---
+++ q16.py
@@ -0,0 +1,7 @@
+# A teacher wants to store marks of students in a list and display the average marks.
+
+marks = [85, 90, 78, 92, 88]
+
+average = sum(marks) / len(marks)
+print("Marks of students:", marks)
+print("Average marks:", average)
