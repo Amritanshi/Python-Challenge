e339faca788b9d99979364e6c40bd0797f41a304
Amritanshi <145573325+Amritanshi@users.noreply.github.com>
2026-05-22 12:33:45 +0530

    Add files via upload

---
+++ q32.py
@@ -0,0 +1,7 @@
+
+# Write a program to display multiplication tables using nested loops.
+
+for i in range(1, 11):       
+    for j in range(1, 11):   
+        print(i, "x", j, "=", i * j)
+    print()  
